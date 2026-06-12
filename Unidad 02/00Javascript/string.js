18d6b66e43a9615723445fee71d634996333e24a
FranciscoChamba <francisco.chamba@unl.edu.ec>
2026-06-12 13:11:01 -0500

    Feat: Learning JavaScript for noobscis

---
+++ Unidad 02/00Javascript/string.js
@@ -0,0 +1,15 @@
+let string = 'Hola, Mundo'
+ let string2 = "Javascript es chevere!"
+let string3 = `${string} feliz :D`
+let string4 = string + ' ' + string2
+
+console.log(string)
+console.log(string2) 
+console.log(string3)
+console.log(string4)
+
+let frase = 'JavaScript es Genial'
+console.log(frase.length) 
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
+console.log(frase.substring(0, 10)) 
---
+++ Unidad 02/00Javascript/tipos-datos.js
@@ -0,0 +1,26 @@
+//PRIMITIVOS
+let nombre = 'Francis'; //String
+let edad = 25; //Number
+let esMAyorDeEdad = true; //Boolean
+let noHayValor = null; //Null
+let noDefinido = undefined; //Undefined
+
+//symbol valor unico
+let simboloUnico = Sumbol('unico')
+
+//datos gigantes bigint
+let numeroGrande = 2n  
+
+
+//COMPLETOS
+//object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S',
+}
+
+//arrays
+let frutas = ['manzana', 'banana', 'naranja']
+
+//functions
+function saludar(){} 
