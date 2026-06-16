b3a55f643c78b7958c0b06202442399baa80ff28
FranciscoChamba <francisco.chamba@unl.edu.ec>
2026-06-16 11:36:36 -0500

    feat:Clase del martes 16 de junio de 2026, nombre:Francisco Chamba

---
+++ Unidad 02/00Javascript/clase-conversion.js
@@ -0,0 +1,11 @@
+const string = '47'
+const insteger = parseInt(string)
+console.log(typeof insteger) 
+
+const decimal = '7.41'
+const float = parseFloat(decimal)
+console.log(typeof float)
+
+const binary = '1010'
+const decimal = parseInt(binary, 2)
+console.log(decimal)
