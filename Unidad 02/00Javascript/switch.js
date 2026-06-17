63324b80b2419cdc1e89778913b518e2429d8cee
FranciscoChamba <francisco.chamba@unl.edu.ec>
2026-06-17 11:21:36 -0500

    Fix: Clase del dia 17-06-2026, Nombre: Francisco Chamba

---
+++ Unidad 02/00Javascript/switch.js
@@ -0,0 +1,20 @@
+let expr = "mangos"
+
+
+switch (expresion) {
+    case "mangos":
+        console.log("los manngos x5 cuestan $1")
+        break;
+    case "naranjas":
+        console.log("las naranjas x10 cuestan $1")
+        break;
+case "manzanas":
+    console.log ("las manzanas x5 cuestan $1")
+    break;
+
+    default:
+        console.log( `Lo siento no contamos con  ${expr}`)
+        break;
+}
+
+console.log("Desea comprar algo adicinal?")
